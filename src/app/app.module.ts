1a81fc850496f55ef22dd35014a4372af32d6c52
francescomerighi <francesco.merighi@marconirovereto.it>
2019-11-05 14:50:13 +0100

    3 files modified, 3 added, 5 deleted

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,11 @@ import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
-import { HelloComponent } from './hello.component';
+import { CalcolatriceComponent } from './calcolatrice/calcolatrice.component';
 
 @NgModule({
   imports:      [ BrowserModule, FormsModule ],
-  declarations: [ AppComponent, HelloComponent ],
+  declarations: [ AppComponent, CalcolatriceComponent ],
   bootstrap:    [ AppComponent ]
 })
 export class AppModule { }
---
+++ src/app/calcolatrice/calcolatrice.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-calcolatrice',
+  templateUrl: './calcolatrice.component.html',
+  styleUrls: ['./calcolatrice.component.css']
+})
+export class CalcolatriceComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
