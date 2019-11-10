b7231958715a41dfbae8bc74bc7017928c54f375
francescomerighi <francesco.merighi@marconirovereto.it>
2019-11-10 13:40:40 +0100

    1 file modified

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -14,11 +14,6 @@ export class AppComponent  {
   }
 
   visualizza(valore:string) {
-    var lastChar: string = this.espressione.substr(this.espressione.length - 1, this.espressione.length)
-    this.espressione = this.espressione + valore;
-    if (this.doppioOperatore(valore) && this.doppioOperatore(lastChar)) {
-      valore = ""
-    }
     this.espressione += valore;
   }
 
@@ -29,9 +24,5 @@ export class AppComponent  {
 
   invertiSegno() {
     this.espressione = "-" + this.espressione;
-  }
-
-  doppioOperatore(char: string) {
-    return char == "+" || char == "-" || char == "x" || char == "/" ? true : false;
   }
 }
